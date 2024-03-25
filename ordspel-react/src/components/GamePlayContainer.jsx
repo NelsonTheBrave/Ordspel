79f2add30da41c0b25e5cc6e0a7b7a48f9751029
Nelson <sherman.erik@gmail.com>
2024-03-25 16:47:39 +0100

    began creating app with react

---
+++ ordspel-react/src/components/GamePlayContainer.jsx
@@ -0,0 +1,11 @@
+import WordRow from './WordRow';
+
+export default function GamePlayContainer() {
+  return (
+    <div>
+      <h1>Let's go!</h1>
+      <p>Start guessing a word</p>
+      <WordRow />
+    </div>
+  );
+}
