1366dce6f33d731984f2240acd9db8bd8ff74478
Nelson <sherman.erik@gmail.com>
2024-04-03 10:03:28 +0200

    updated functionality to meet test criteria and added Richards example test

--- evaluateGuess.js
+++ evaluateGuess.js
@@ -1,29 +1,58 @@
 export function evaluateGuess(guessedWord, targetWord) {
   const output = [];
   for (let i = 0; i < guessedWord.length; i++) {
-    const guessedLetter = guessedWord.charAt(i);
-    const targetLetter = targetWord.charAt(i);
-    const result = matchingStatus();
+    output.push('');
+  }
+  checkForCorrect();
+  checkForMisplaced();
+  markIncorrect();
 
-    function matchingStatus() {
+  function checkForCorrect() {
+    for (let i = 0; i < guessedWord.length; i++) {
+      const guessedLetter = guessedWord.charAt(i);
+      const targetLetter = targetWord.charAt(i);
       if (guessedLetter == targetLetter) {
-        return 'correct';
-      } else {
-        for (let j = 0; j < targetWord.length; j++) {
-          if (guessedLetter == targetWord.charAt(j)) {
-            targetWord = targetWord.replace(targetWord.charAt(j), '?'); // "raderar" bokstaven från mål-ordet så att samma bokstav inte kan resultera i "misplaced" fler än en gång
-            return 'misplaced';
-          }
+        output[i] = { letter: guessedLetter, result: 'correct' };
+        updateGuessedWord(i);
+        updateTargetWord(i);
+      }
+    }
+  }
+  function checkForMisplaced() {
+    for (let i = 0; i < guessedWord.length; i++) {
+      const guessedLetter = guessedWord.charAt(i);
+      for (let j = 0; j < targetWord.length; j++) {
+        if (guessedLetter == targetWord.charAt(j)) {
+          output[i] = { letter: guessedLetter, result: 'misplaced' };
+          updateGuessedWord(i);
+          updateTargetWord(j);
         }
       }
-      return 'incorrect';
     }
-
-    const letterObject = { letter: guessedLetter, result: result };
-    output.push(letterObject);
   }
+  function markIncorrect() {
+    for (let i = 0; i < guessedWord.length; i++) {
+      const guessedLetter = guessedWord.charAt(i);
+      if (guessedLetter != '!') {
+        output[i] = { letter: guessedLetter, result: 'incorrect' };
+      }
+    }
+  }
+  function updateGuessedWord(index) {
+    guessedWord = guessedWord.split('');
+    guessedWord.splice(index, 1, '!');
+    guessedWord = guessedWord.join('');
+  }
+  function updateTargetWord(index) {
+    targetWord = targetWord.split('');
+    targetWord.splice(index, 1, '?');
+    targetWord = targetWord.join('');
+  }
+  console.log(output);
   return output;
 }
+
+// evaluateGuess('a', 'a');
 
 /* 
 DEFINIERA
